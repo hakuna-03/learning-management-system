b491a3206e4c0dbf240a83275ff00d311963242c
EmanMK <eman.elokisy@gmail.com>
2023-09-04 09:37:32 +0300

    feature():error-handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
 const { swaggerFile } = require("./utils/swagger");
+const { globalError } = require("./middlewares/error-middleware");
 
 const app = express();
 dotenv.config({ path: `${__dirname}/../.env` });
-
-console.log(swaggerFile);
 
 app.use(express.json());
 
 // Serve the Swagger UI.
 app.get("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-const { globalError } = require("./middlewares/error-middleware");
-
-
-app.get("/login", (req, res) => {
+// testing purpose
+app.get("/", (req, res) => {
   res.send("<h1>Hakuna Matata!</h1>");
 });
 
